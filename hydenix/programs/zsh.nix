4b28598e9360c1a703810d79ad267294190330fa
He Wentao <goodmorning_hwt@sjtu.edu.cn>
2025-03-07 13:00:04 +0800

    feat: removing omz and p10k

--- hydenix/programs/zsh.nix
+++ hydenix/programs/zsh.nix
@@ -8,11 +8,11 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    oh-my-zsh = {
-      enable = true;
-    };
+    # oh-my-zsh = {
+    #   enable = true;
+    # };
     initExtra = ''
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      # source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
 
       # Helpful aliases
       alias c='clear' # clear terminal
@@ -34,21 +34,21 @@
       alias mkdir='mkdir -p'
 
       # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh  
+      # [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh  
 
       source $HOME/.local/state/nix/profiles/home-manager/home-path/share/hyde/hyde-cli/shell-completions/Hyde.zsh
 
       export PATH=$HOME/.local/bin:$PATH
     '';
     initExtraFirst = ''
-      #Display Pokemonks
-      pokemon-colorscripts --no-title -r 1-3
+      # Display Pokemonks
+      # pokemon-colorscripts --no-title -r 1-3
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
       # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
+      # if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+      #   source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      # fi
     '';
   };
 }
